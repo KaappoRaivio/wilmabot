e50fd988b52bbcd517c900145e4151d5216d8c50
NoSocksForYou <kaappo.raivio@gmail.com>
2018-03-30 19:18:29 +0300

    started on a new approach to the schedule.

---
+++ javascript/find_schedule.py
@@ -0,0 +1,61 @@
+import re
+
+# string = open('filu.html', 'r').read()
+#
+# pattern_alku = re.compile('<script type="text/javascript">')
+# pattern_loppu = re.compile('var weekdays')
+#
+# lista = string.split('\n')
+#
+# a = 0
+#
+# for i in lista:
+#     if pattern_alku.search(i):
+#         lista = lista[a:]
+#         break
+#     # å = print(pattern_loppu.search(i).group(), a) if pattern_loppu.search(i) is not None else None
+#     a += 1
+#
+# print(lista)
+#
+# a = 0
+#
+# for i in lista:
+#     if pattern_loppu.search(i):
+#         lista = lista[:a]
+#         break
+#     # å = print(pattern_loppu.search(i).group(), a) if pattern_loppu.search(i) is not None else None
+#     a += 1
+#
+# print('\n'.join(lista))
+
+def find(string):
+    pattern_alku = re.compile('<script type="text/javascript">')
+    pattern_loppu = re.compile('var weekdays')
+
+    lista = string.split('\n')
+
+    a = 0
+
+    for i in lista:
+        if pattern_alku.search(i):
+            lista = lista[a:]
+            break
+        # å = print(pattern_loppu.search(i).group(), a) if pattern_loppu.search(i) is not None else None
+        a += 1
+
+    # print(lista)
+
+    a = 0
+
+    for i in lista:
+        if pattern_loppu.search(i):
+            lista = lista[:a]
+            break
+        # å = print(pattern_loppu.search(i).group(), a) if pattern_loppu.search(i) is not None else None
+        a += 1
+
+    return '\n'.join(lista)
+
+if __name__ == '__main__':
+    print(find(open('filu.html', 'r').read()))
--- javascript/testi.py
+++
@@ -1,9 +0,0 @@
-import re
-
-string = open('filu.html', 'r').read()
-
-pattern_alku = re.compile('      <script type="text/javascript">')
-pattern_loppu = re.compile('[\t ]*</script>')
-
-for i in string.split('\n'):
-    a = print(pattern_alku.search(i)) if pattern_alku.search(i) is not None else None
